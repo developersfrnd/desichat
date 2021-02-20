c8813b301a17fb4779371351770bbd9188c68dad
developersfrnd <developersfrnd@gmail.com>
2021-02-20 20:41:53 +0530

    convert class component to functional component

--- src/ApiManager/auth.js
+++ src/ApiManager/auth.js
@@ -1,15 +1,14 @@
 import Constants from "../Config/Constants";
-import usersModel from "./user";
 
 const authModel = {
 
     getAuthToken: () => {
-        let dscAuthObj = localStorage.getItem('dsc-auth');
+        let dscAuthObj = sessionStorage.getItem('dsc-auth');
         return (dscAuthObj) ? JSON.parse(dscAuthObj).token : null;
     },    
 
     getAuthUserObj: () => {
-        return (localStorage.getItem('dsc-auth')) ? JSON.parse(localStorage.getItem('dsc-auth')) : null;
+        return (sessionStorage.getItem('dsc-auth')) ? JSON.parse(sessionStorage.getItem('dsc-auth')) : null;
     },
 
     isModelLogin: function(){
@@ -23,14 +22,14 @@ const authModel = {
     setAuthToken: (authUser) => {
 
         let obj = {'token':authUser.token,'id':authUser.id,'role':authUser.role,'name':authUser.name}
-        return localStorage.setItem('dsc-auth',JSON.stringify(obj));
+        return sessionStorage.setItem('dsc-auth',JSON.stringify(obj));
     },
 
     getAuthHeaders: function(){
         return (this.getAuthToken()) ? {Authorization: `Bearer ${this.getAuthToken()}`} : {};
     },
     removeAuthToken: () => {
-        return localStorage.removeItem('dsc-auth');
+        return sessionStorage.removeItem('dsc-auth');
     },
 }
 
